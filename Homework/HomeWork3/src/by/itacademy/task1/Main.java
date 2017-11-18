eb7865db6ac7a2c5ef64cc430f1051cf12a82b30
AnnaOlehovna <anya.olehovna@gmail.com>
2017-11-18 21:21:50 +0300

    Good Start!
    Signed-off-by: AnnaOlehovna <anya.olehovna@gmail.com>

--- Homework/HomeWork3/src/by/itacademy/task1/Main.java
+++ Homework/HomeWork3/src/by/itacademy/task1/Main.java
@@ -1,7 +1,33 @@
 package by.itacademy.task1;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] arg){
-        
+
+
+    public static void main(String[] arg) {
+        int[] currentArray = getTenNumbers();
+        printArray(currentArray);
+
     }
+
+    public static int[] getTenNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int[] arrayOfTenNumbers = new int[10];
+        for (int i = 0; i < arrayOfTenNumbers.length; i++) {
+            System.out.println("Введите " +(i+1)+ "-ое целое число");
+            int number = scanner.nextInt();
+            arrayOfTenNumbers[i] = number;
+        }
+        return arrayOfTenNumbers;
+    }
+
+    public static void printArray(int[] arrayForPrinting) {
+        int[] tempArray = arrayForPrinting;
+        for (int i = 0; i < tempArray.length; i++) {
+            System.out.print(tempArray[i] + " | ");
+        }
+    }
+
+
 }
