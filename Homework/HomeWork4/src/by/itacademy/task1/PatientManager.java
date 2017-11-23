ef775cb1ff1404ac640bfcf88af8a596a96a2059
AnnaOlehovna <anya.olehovna@gmail.com>
2017-11-23 22:58:44 +0300

    trying homework4
    Signed-off-by: AnnaOlehovna <anya.olehovna@gmail.com>

---
+++ Homework/HomeWork4/src/by/itacademy/task1/PatientManager.java
@@ -0,0 +1,18 @@
+package by.itacademy.task1;
+
+public class PatientManager {
+
+    private Patient[] patients;
+
+    public PatientManager(Patient[] patients) {
+        this.patients = patients;
+    }
+
+    public Patient[] getPatients() {
+        return patients;
+    }
+
+
+
+
+}
