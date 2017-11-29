e453b3419fc87ef2a8fad0a4fa33728a969e2f84
AnnaOlehovna <anya.olehovna@gmail.com>
2017-11-29 09:44:02 +0200

    task 3 done!

--- Homework/HomeWork6/src/by/itacademy/task3/Main.java
+++ Homework/HomeWork6/src/by/itacademy/task3/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         Shape shape;
-        shape=new Triangle(4,5);
+        shape=new Triangle(4,5,Math.PI/6);
         System.out.println(shape.calculateSq());
 
-        shape=new RightTriangle(4,(Math.PI)/6);
+        shape = new Rectangle(6,7);
+        System.out.println(shape.calculateSq());
+
+        shape = new Square(8);
+        System.out.println(shape.calculateSq());
+
+        shape = new Circle(4);
         System.out.println(shape.calculateSq());
 
     }
--- Homework/HomeWork6/src/by/itacademy/task3/Triangle.java
+++ Homework/HomeWork6/src/by/itacademy/task3/Triangle.java
@@ -3,16 +3,18 @@ package by.itacademy.task3;
 public class Triangle implements Shape{
 
     private double aSide;
-    private double height;
+    private double bSide;
+    private double angle;
 
-    public Triangle(double aSide, double height) {
+    public Triangle(double aSide, double bSide, double angle) {
         this.aSide = aSide;
-        this.height = height;
+        this.bSide = bSide;
+        this.angle = angle;
     }
 
     @Override
     public double calculateSq() {
-        double triangleSq=0.5*aSide*height;
+        double triangleSq=0.5*aSide*bSide*Math.sin(angle);
         return triangleSq;
     }
 
