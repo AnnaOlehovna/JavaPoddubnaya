0337b8348dbd8f2fafc78c2cde25e736902cf43a
AnnaOlehovna <anya.olehovna@gmail.com>
2017-12-10 19:25:34 +0300

    task1 HomeWork9 !
    Signed-off-by: AnnaOlehovna <anya.olehovna@gmail.com>

---
+++ Homework/HomeWork9/src/by/itacademy/task1/Main.java
@@ -0,0 +1,40 @@
+package by.itacademy.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Создание и заполнение ArrayList
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean isFill = true;
+       do try {
+           System.out.println("Введите строку для добавления в массив или q - для выхода");
+            String str = reader.readLine();
+            if(str.equals("q")){
+                isFill=false;
+                System.out.println("Заполнение завершено");
+            }else{
+                list.add(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }while(isFill);
+
+       //Печать list
+        System.out.println(list.toString());
+
+        //Удаление букв а(кирилица) из строк
+        for(int i=0; i<list.size();i++){
+           String str=list.get(i).replace("а","");
+           list.set(i,str);
+        }
+
+        //Печать list без букв а ='(
+        System.out.println(list.toString());
+    }
+}
