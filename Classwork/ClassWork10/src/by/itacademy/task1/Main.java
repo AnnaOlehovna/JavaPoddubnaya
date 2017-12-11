6e887c5e0d02239c3c2420884e5a7d9f48dcc7d1
Anna Poddubnaya <anya.olehovna@gmail.com>
2017-12-11 21:05:16 +0300

    ClassWork10

--- Classwork/ClassWork10/src/by/itacademy/task1/Main.java
+++ Classwork/ClassWork10/src/by/itacademy/task1/Main.java
@@ -1,10 +1,49 @@
 package by.itacademy.task1;
 
+import java.util.*;
+
 /**
  * Created by user on 11.12.2017.
  */
 public class Main {
     public static void main(String[] args) {
+
+        Set<String> testSet = new HashSet<>();
+//        testSet.add("AAA");
+//        testSet.add("SSS");
+//        testSet.add("AAA");
+//        for(String  test: testSet){
+//            System.out.println("value = "+test);
+//        }
+
+        List<String > list = new ArrayList<>();
+        list.add("AAA");
+        list.add("SSS");
+        list.add("AAA");
+
+        testSet.addAll(list);
+        list.clear();
+        list.addAll(testSet);
+
+
+        Set<Student> testSet2 = new HashSet<>();
+        testSet2.add(new Student("AAA",25));
+        testSet2.add(new Student("DDD",20));
+        testSet2.add(new Student("AAA",28));
+
+        //Ключ не может повторяться - он определящиюй
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("RU"," RU Value");
+        testMap.put("BY","BY Value2");
+        testMap.put("EN","EN Value");
+        testMap.put("FR","FR Value3");
+
+        System.out.println("VALUE = "+testMap.get("RU"));
+
+
+
+
+
 
     }
 }
---
+++ Classwork/ClassWork10/src/by/itacademy/task1/Student.java
@@ -0,0 +1,66 @@
+package by.itacademy.task1;
+
+/**
+ * Created by user on 11.12.2017.
+ */
+public class Student {
+        private String name;
+        private int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+//    //одинаковые студенты, если имена одинаковые, на возраст не обращаем значение
+//    @Override
+//    public boolean equals(Object obj) {
+//        Student student = (Student)obj;
+//        return name.equals(student.getName());
+//    }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Student student = (Student) o;
+
+            if (age != student.age) return false;
+            return name != null ? name.equals(student.name) : student.name == null;
+        }
+        //if(name != null){result = name.hashCode()} else{result =0} - расшифровка второй строки в методе, там сокращенный if
+        //hashcode - чаще всего уникальный int код, но из-за конечных границ int  - могут случаться повторения
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + age;
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
